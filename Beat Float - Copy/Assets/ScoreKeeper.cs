e7a6029c60cfc35c846b81bc7949fd02270ac598
alexbingham18 <bingham-a8@ulster.ac.uk>
2019-03-19 17:59:37 +0000

    UI

---
+++ Beat Float - Copy/Assets/ScoreKeeper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public static ScoreKeeper instance;
+
+    public int score;
+	void Awake () {
+        instance = this;
+	}
+	
+
+	public void AddScore (int s) {
+        score = score + s;
+	}
+    public int GetScore()
+    {
+        return score;
+    }
+}
