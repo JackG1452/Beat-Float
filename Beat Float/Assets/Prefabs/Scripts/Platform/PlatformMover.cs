b4b6c98e046ab8a54f17c755180d9ffb572d8f41
Szymon <karas-s@ulster.ac.uk>
2019-03-19 17:39:18 +0000

    Code for platforms

---
+++ Beat Float/Assets/Prefabs/Scripts/Platform/PlatformMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    [Range(0,20)]
+    public float speed = 1f;
+    
+    public int endPosZ;
+
+    private bool hitEnd = false;
+
+    void Awake()
+    {
+        //create random position (always same z position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+
+        if(gameObject.transform.position.z < endPosZ)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
