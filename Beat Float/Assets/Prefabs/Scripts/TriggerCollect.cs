f6a4c0c5a3ac03862fb4f0e685cd99186e757cd9
jack <gillespie-j20@ulster.ac.uk>
2019-03-19 17:30:06 +0000

    Added Art Assets

---
+++ Beat Float/Assets/Prefabs/Scripts/TriggerCollect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCollect : MonoBehaviour {
+
+	[Header ("ITEM VALUE")]
+	[Range(1,10)]
+	public int gemValue = 1;
+
+	[Space(5f)]
+	public GameObject collectParticles;
+
+	void Start () {
+
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			Debug.Log ("Item Collected");
+			CollectItem ();
+			CreateParticles ();
+
+			//find the audiomanager and play the sound
+            //Either needs added/changed or deleted completely depends on if we have a collect sound
+			//GameObject.FindWithTag ("AudioManager").GetComponent<AudioManager> ().PlayCollect ();
+
+			Destroy (gameObject);
+		}
+	}
+
+	private void CollectItem(){
+		//add gems to the score keeper class
+        //This would need to be implemented or changed to suit
+		//ScoreKeeper.instance.SetGemsCollected(gemValue);
+
+		//disable the collider
+		gameObject.GetComponent<Collider2D> ().enabled = false;
+		//disable the renderer
+		gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+
+	}
+	private void CreateParticles(){
+		//instantiate a particle system at the objects position & rotation
+		GameObject particlePrefab = Instantiate (collectParticles, transform.position, Quaternion.identity);
+	}
+}
